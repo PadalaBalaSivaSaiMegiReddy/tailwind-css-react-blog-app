b4622f3cfc772df6dd5d26f8cab0c6a4a298141e
Bala Siva Sai Megi Reddy Padala <megireddy21@gmail.com>
2023-06-04 15:00:12 +0530

    header  done almost maybe

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,41 @@
 import React from "react";
+import images from "../constants/images";
 
 export default function Header() {
 	return (
 		<section>
-			<header></header>
+			<header className="container mx-auto px-5 flex justify-between py-4 items-center ">
+				<div>
+					<img src={images.Logo} alt="logo" />
+				</div>
+				<div className="flex gap-x-9 items-center">
+					<ul className="flex gap-x-5 font-semibold ">
+						<li>
+							<a href="/">Home</a>
+						</li>
+
+						<li>
+							<a href="/">Articles</a>
+						</li>
+
+						<li>
+							<a href="/">Pages</a>
+						</li>
+
+						<li>
+							<a href="/">Pricing</a>
+						</li>
+
+						<li>
+							<a href="/">FAQs</a>
+						</li>
+					</ul>
+
+					<button className="border-2 border-blue-500 px-6 py-3 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300  ">
+						Sign in
+					</button>
+				</div>
+			</header>
 		</section>
 	);
 }
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,1 +1,5 @@
-import logo from '../assets';
+import Logo from '../assets/Logo.svg';
+const images ={
+    Logo,
+}
+export default images;
