4a7cd9e6e69ff1983d3e4935aa74a93f3848f92f
Bala Siva Sai Megi Reddy Padala <megireddy21@gmail.com>
2023-06-10 15:30:50 +0530

    new

---
+++ src/pages/home/container/Articles.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ArticleCard from '../../../components/ArticleCard'
+
+
+
+function Articles() {
+  return (
+		<section className='container flex flex-wrap mx-auto md:gap-x-5 gap-y-5 px-5 py-10 '>
+			<ArticleCard />
+			Articles
+		</section>
+	);
+}
+
+export default Articles
