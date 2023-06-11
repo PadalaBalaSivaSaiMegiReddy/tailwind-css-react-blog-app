e43535dc1b14bb40c9ca085c78ce47c408eb3064
Bala Siva Sai Megi Reddy Padala <megireddy21@gmail.com>
2023-06-11 22:21:05 +0530

    donee

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import images from "../constants/images";
-
+import { BsCheckLg } from "react-icons/bs";
 function ArticleCard({ className }) {
 	return (
 		<div
@@ -9,13 +9,37 @@ function ArticleCard({ className }) {
 			<img
 				src={images.Post1Image}
 				alt="title"
-				className="w-full object-cover object-center h-auto"
+				className="w-full object-cover object-center h-auto md:h-52 lg:h-48 xl:h-60"
 			/>
 			<div className="p-5">
-				<h3 className="font-roboto text-xl font-bold text-dark-soft ">
+				<h3 className="font-roboto font-bold text-xl text-dark-soft md:text-2xl lg:text-[28px] ">
 					Future of work
 				</h3>
-				<p className="text-dark-light mt-3 text-sm">Majority of people will work in jobs that doesn't exist today</p>
+
+				<p className="text-dark-light mt-3 text-sm md:text-lg">
+					Majority of people will work in jobs that doesn't exist today
+				</p>
+				<div className="flex justify-between flex-nowrap items-center mt-6 ">
+					<div className="flex items-center gap-2">
+						<img src={images.PostProfileImage} alt="post profile" />
+						<div className="col flex-col">
+							<h4 className="font-bold italic text-dark-soft text-sm">
+								Viola Manisa
+							</h4>
+						</div>
+						<div className=" flex items-center gap-x-2 md:gap-x-2.5">
+							<span className="bg-[#36B37E] w-fit bg-opacity-20 p-1.5 rounded-full">
+								<BsCheckLg className="w-1.5 h-1.5 text-[#36B37E]" />
+							</span>
+							<span className=" italic text-dark-light text-xs ">
+								Verified writer
+							</span>
+						</div>
+					</div>
+					<span className="font-bold text-dark-light italic text-sm ">
+						02 May
+					</span>
+				</div>
 			</div>
 		</div>
 	);
--- src/pages/home/container/Articles.jsx
+++ src/pages/home/container/Articles.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import ArticleCard from '../../../components/ArticleCard'
-
-
+import React from "react";
+import ArticleCard from "../../../components/ArticleCard";
 
 function Articles() {
-  return (
-		<section className='container flex flex-wrap mx-auto md:gap-x-5 gap-y-5 px-5 py-10 '>
-			<ArticleCard className={"w-full"} />
+	return (
+		<section className="container flex flex-wrap mx-auto md:gap-x-5 gap-y-5 px-5 py-10 ">
+			<ArticleCard className="w-full md:[(calc50%-20px)]" />
+			<ArticleCard className="w-full md:[(calc50%-20px)]" />
 		</section>
 	);
 }
 
-export default Articles
+export default Articles;
