bde7ebe9de05a78d4acefcb73ed95d119fda4fde
Bala Siva Sai Megi Reddy Padala <megireddy21@gmail.com>
2023-06-12 23:39:20 +0530

    changes

--- src/pages/home/container/Articles.jsx
+++ src/pages/home/container/Articles.jsx
@@ -4,6 +4,8 @@ import ArticleCard from "../../../components/ArticleCard";
 function Articles() {
 	return (
 		<section className="container flex flex-wrap mx-auto md:gap-x-5 gap-y-5 px-5 py-10 ">
+			<p>hi</p>
+
 			<ArticleCard className="w-full md:[(calc50%-20px)]" />
 			<ArticleCard className="w-full md:[(calc50%-20px)]" />
 		</section>
